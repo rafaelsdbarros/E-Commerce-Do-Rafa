bf8d8a2f53e821c900b361c43eebe3f756864309
rafaelsdbarros <r4f4el1994@gmail.com>
2024-11-21 11:28:20 -0300

    [feat]: implementação de um modal de apresensação na home ao abrir o site.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 //Contato
+const fullName = "Rafael Barros"
 const telefone = "(11) 91234-5678"
 const whats = "(11) 91234-5678"
 const email = "rafastore@gmail.com"
@@ -11,9 +12,85 @@ const insta = "@rafastore"
 const facebook = "@rafastore"
 const linkedin = "@rafastore"
 
+const date = new Date()
+
 window.addEventListener('load', function() {
+    setupModal()
     loadFooterInfo()
 });
+
+function getWeekDay(weekDay) {
+    switch (weekDay) {
+        case 0:
+            return "Domingo"
+           break; 
+        case 1:
+            return "Segunda-feira"
+           break;
+        case 2:
+            return "Terça-feira"
+           break;   
+        case 3:
+            return "Quarta-feira"
+           break;   
+        case 4:
+            return "Quinta-feira"
+           break;                           
+        case 5:
+             return "Sexta-feira"
+            break;
+        case 6:
+            return "Sabado"
+            break;                
+        default:
+            break;
+    }
+}
+
+function getFullDate() {
+    const day = date.getDate()
+    const month = date.getMonth()
+    const year = date.getFullYear()
+    const weekDay = date.getDay()
+
+    const fullDate = `${day}/${month}/${year} - ${getWeekDay(weekDay)}`
+    return fullDate
+}
+
+function setupModal() {
+
+    var modal = document.getElementById("myModal");
+    var span = document.getElementsByClassName("close")[0];
+
+    //close modal on click close.
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(e){
+        if (e.target ==  modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    //Display content
+    const divContent = document.getElementById("divContent");
+    divContent.innerHTML = 
+    `   
+        <br>
+        <hr>
+        <br>
+        <p>Sou <b> ${fullName} </b> criador deste site, é um prazer receber-los(as)! </p>
+        <p>Formando em ADS/Mackenzie</p>
+        <p>Hoje é ${getFullDate()}</p>
+        <br>
+        <i><strong>Desejo ótimas compras :) </strong></i>
+        <br>
+        <br>
+        <hr>
+    `
+    modal.style.display = "block"
+}
 
 function loadFooterInfo() {
 
